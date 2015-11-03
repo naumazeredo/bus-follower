a2f117872601f4acd0aa78b1e62832547aec97d0
athos <athos@vialink.com.br>
2015-11-02 22:11:11 -0200

    Requesting the CSV and converting it to JS object.

--- client/scripts/form.js
+++ client/scripts/form.js
@@ -10,6 +10,27 @@ Template.busQuery.events({
             }
             update = false
 
+            $.get("http://dadosabertos.rio.rj.gov.br/apiTransporte/Apresentacao/csv/gtfs/onibus/percursos/gtfs_linha"+bus+"-shapes.csv", function(data) {
+                function csvJSON(csv){
+                    var lines=csv.split("\n");
+                    var result = [];
+                    var headers=lines[0].split(",");
+
+                    for(var i=1;i<lines.length;i++){
+                        var obj = {};
+                        var currentline=lines[i].split(",");
+                        if (currentline.length == headers.length) {
+                            for(var j=0;j<headers.length;j++){
+                                obj[headers[j]] = currentline[j].replace('/\r/g', '').replace('/"/g', '');;
+                            }
+                            result.push(obj);
+                        }
+                    }
+                    return result;
+                }
+
+                data  = csvJSON(data)
+            })
             var getBuses = function() {
                 $.get("http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/onibus/"+bus, function(data) {
                     curRoute.updateBuses(data.DATA);
